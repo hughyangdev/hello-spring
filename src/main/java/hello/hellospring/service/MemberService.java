de2c3daa9e081b5d9248f2f90611aaf62785ad64
hughyang <hughyangdev@gmail.com>
2021-09-17 17:01:11 +0900

    add MemberServiceTest refactor: edit MemberService
    회원 서비스 테스트 작성
    서비스와 서비스 테스트에서 동일 객체 사용을 위해 DI로 변경

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,21 +9,28 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;    // = new MemoryMemberRepository();
+
+    // service에서 repository 객체를 생성하지 않고 생성자를 통해 외부에서 넣어서 사용
+    // -> DI(Dependency Injection, 의존성 주입)
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원 가입
      */
     public Long join(Member member) {
         // 같은 이름이 있는 중복 회원 허용 안함
-        // null 가능성이 있으면 Optional로 감싸서 반환해주며 이로 인핸 ifPresent 사용 가능
+        // null 가능성이 있으면 Optional로 감싸서 반환해주며 이로 인해 ifPresent 사용 가능
+        // Optional로 감싸지 않으면 null 체크하여야 함
 //        Optional<Member> result = memberRepository.findByName(member.getName());
-//        result.ifPresent(m -> { // ifPresent: 값이 있다면, m: member, optional이라서 null 확인 안함
+//        result.ifPresent(m -> { // ifPresent: 값이 있다면, m: member, Optional이라서 null 확인 안함
 //            throw new IllegalStateException("이미 존재하는 회원입니다.");
 //        });
         // Optional<Member> 반환을 result로 받지 않고 바로 메서드 실행으로 코드 정리
         // 메서드로 별도 분리 refatoring 진행
-        //  -> 영역 선택 후 ctrl+alt+shift+T 에서 extract method || ctrl+alt+m 으로 메서드 분리 생성  
+        //  -> 영역 선택 후 ctrl+alt+shift+T 에서 extract method OR ctrl+alt+m 으로 메서드 분리 생성
         validateDuplicateMember(member);    // 중복 회원 검증
 
         memberRepository.save(member);
--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,4 +1,94 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+
 class MemberServiceTest {
-  
+
+    MemberService memberService;    // = new MemberService();
+    // clear를 해줘야하는데 service만 있는 상태 -> MemoryMemberRepository를 생성하여 clearStroe 사용
+    MemoryMemberRepository memberRepository;    // = new MemoryMemberRepository();
+    // 테스트에서 memberRepository는 service의 memberRepository와 다른 객체
+    // -> 현재는 MemoryMemberRepository의 store가 static이라 괜찮지만 static이 아닌 경우 문제 발생 가능
+    // service에 생성자를 만들어서 외부에서 MemberRepository를 넣어주게 해서 동일한 객체를 사용하게 한다. @BeforeEach로 객체 생성 및 주입
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    // 각 test가 끝난 후 저장소를 비워주기 위해 작성
+    // MemoryMemberRepository 에 clearStore() 작성
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void join() {
+        // given
+        Member member = new Member();
+        member.setName("spring");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        /*try {
+            memberService.join(member2);
+            fail();
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }*/
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+        // then
+    }
+
+    @Test
+    void findMembers() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        memberService.join(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        memberService.join(member2);
+
+        List<Member> result = memberService.findMembers();
+
+        assertThat(result.size()).isEqualTo(2);
+
+    }
+
+    @Test
+    void findOne() {
+    }
 }
